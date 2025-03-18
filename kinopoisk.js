51c1e10e278dda7fedbf11470ba63ec8572f116f
Revan123 <magus191995@gmail.com>
2025-03-19 02:45:37 +0300

    Update kinopoisk.js

--- kinopoisk.js
+++ kinopoisk.js
@@ -3,20 +3,24 @@
         name: 'KP Rating on Cards',
         version: '1.0',
         run: function() {
-            // Перехватываем событие отрисовки карточек
-            Lampa.Listener.follow('hover', function(e) {
-                var item = e.data; // Данные фильма/сериала
-                var element = e.element; // DOM-элемент карточки
+            // Проверка авторизации
+            var token = Lampa.Storage.get('kinopoisk_token', '');
+            if (!token) {
+                Lampa.Noty.show('Авторизуйтесь в Кинопоиске через настройки плагина');
+                return;
+            }
 
-                // Проверяем наличие рейтинга Кинопоиска
-                var kp_rating = item.ratingKinopoisk || item.kp_rating || 'N/A';
-                if (kp_rating && kp_rating !== 'N/A') {
-                    // Находим блок рейтинга TMDB
+            // Перехват отрисовки карточек в списке
+            Lampa.Listener.follow('hover', function(e) {
+                var item = e.data;
+                var element = e.element;
+
+                // Предполагаем, что рейтинг уже доступен как item.ratingKinopoisk
+                var kp_rating = item.ratingKinopoisk || 'N/A';
+                if (kp_rating !== 'N/A') {
                     var rating_block = element.find('.card__rating');
                     if (rating_block.length) {
-                        // Удаляем старый рейтинг KP, если он уже есть (чтобы не дублировать)
                         rating_block.find('.kp-rating').remove();
-                        // Добавляем рейтинг Кинопоиска рядом с TMDB
                         rating_block.append(
                             '<span class="kp-rating" style="margin-left: 5px; color: #f5c518; font-size: 12px; font-weight: bold;">KP: ' + kp_rating + '</span>'
                         );
@@ -26,7 +30,6 @@
         }
     };
 
-    // Регистрация плагина
     if (window.Lampa) {
         window.Lampa.Plugin.add(plugin);
     }
